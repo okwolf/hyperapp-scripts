3679e9d1799098be352fb78f4d5fc4647dc5ad36
Wolfgang <wolf@okwolf.com>
2018-11-03 18:36:13 -0700

    add example app to make testing cra changes easier

---
+++ example/src/App.js
@@ -0,0 +1,17 @@
+import { h } from "hyperapp";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to Hyperapp</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+export default App;
